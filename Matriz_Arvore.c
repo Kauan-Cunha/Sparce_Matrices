6fe10493ce9b7a0784cb0b17883f9b7a1af57e8a
lucasvaladareslima <l247025@dac.unicamp.br>
2025-11-15 18:12:24 -0300

    substitui vetor de AVL por AVL de AVL e implementa criar_matriz_arvore

--- Matriz_Arvore.c
+++ Matriz_Arvore.c
@@ -1,6 +1,7 @@
+#include <stdlib.h>
 #include "Matriz_Arvore.h"
 
-int buscar_entrada(p_avl arvore, int j)
+int buscar_entrada(p_avl_linha arvore, int j)
 {
     if (arvore->j == j)
     {
@@ -11,4 +12,24 @@ int buscar_entrada(p_avl arvore, int j)
         return buscar_entrada(arvore->dir, j);
     }
     return buscar_entrada(arvore->esq, j);
+}
+
+p_avl_linha criar_matriz_arvore()
+{
+    p_avl_linha a = malloc(sizeof(AVL_Linha));
+    a->j = -1;
+    a->col = NULL;
+    a->esq = NULL;
+    a->dir = NULL;
+    return a;
+}
+
+p_avl_linha copiar(p_avl_linha a)
+{
+    p_avl_linha b = criar_matriz_arvore();
+}
+
+int somar(p_avl_linha a, p_avl_linha b)
+{
+    p_avl_linha c = criar_matriz_arvore();
 }
