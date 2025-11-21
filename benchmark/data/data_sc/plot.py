4b505ffa1aae0234086138dfa74c27b49ef64d4c
Kauan Cunha da Silva <240030@dac.unicamp.br>
2025-11-21 20:20:30 -0300

    Finalizei o plot dos benchmarks e organizei as pastas.

--- benchmark/data/data_sc/plot.py
+++ benchmark/data/data_sc/plot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # 1. Carregar CSV
 # -----------------------------------------------------------
 # Salve os dados acima em: dados.csv
-data = np.genfromtxt("hash_insercao_esp_dez.csv", delimiter=",", names=True)
+data = np.genfromtxt("hash_mult_esp_dez.csv", delimiter=",", names=True)
 
 x = data["Num_Elementos"]
 y = data["Tempo_Medio_Segundos"]
@@ -17,7 +17,7 @@ plt.figure(figsize=(8,5))
 plt.plot(x, y, 'o-', label="Dados")
 plt.xlabel("Número de Elementos")
 plt.ylabel("Tempo Médio (s)")
-plt.title("Multiplicação Escalar AVL: Tempo Médio x Número de elementos não-nulos")
+plt.title("Mult. Escalar hash: Tempo Médio x Número de elementos não-nulos")
 plt.grid(True)
 plt.legend()
 plt.tight_layout()
@@ -27,37 +27,37 @@ plt.show()
 # -----------------------------------------------------------
 # 3. Ajuste logarítmico (y = a*log(x) + b)
 # -----------------------------------------------------------
-logx = np.log(x)
+# logx = np.log(x)
 
-# Ajuste linear em log(x)
-coef = np.polyfit(logx, y, 1)  
-a, b = coef
+# # Ajuste linear em log(x)
+# coef = np.polyfit(logx, y, 1)  
+# a, b = coef
 
-# Curva ajustada
-y_fit = a * logx + b
+# # Curva ajustada
+# y_fit = a * logx + b
 
-plt.figure(figsize=(8,5))
-plt.plot(x, y, 'o', label="Dados reais")
-plt.plot(x, y_fit, '-', label=f"Fit: y = {a:.2e} * ln(x) + {b:.2e}")
-plt.xlabel("Número de Elementos")
-plt.ylabel("Tempo Médio (s)")
-plt.title("Ajuste Logarítmico")
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(8,5))
+# plt.plot(x, y, 'o', label="Dados reais")
+# plt.plot(x, y_fit, '-', label=f"Fit: y = {a:.2e} * ln(x) + {b:.2e}")
+# plt.xlabel("Número de Elementos")
+# plt.ylabel("Tempo Médio (s)")
+# plt.title("Ajuste Logarítmico")
+# plt.grid(True)
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
 
 
 # -----------------------------------------------------------
 # 4. Escala logarítmica no eixo X
 # -----------------------------------------------------------
-plt.figure(figsize=(8,5))
-plt.plot(x, y, 'o-', label="Dados")
-plt.xscale("log")
-plt.xlabel("Número de Elementos (escala log)")
-plt.ylabel("Tempo Médio (s)")
-plt.title("Tempo Médio vs Número de Elementos (Escala Log)")
-plt.grid(True, which="both")
-plt.legend()
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(8,5))
+# plt.plot(x, y, 'o-', label="Dados")
+# plt.xscale("log")
+# plt.xlabel("Número de Elementos (escala log)")
+# plt.ylabel("Tempo Médio (s)")
+# plt.title("Tempo Médio vs Número de Elementos (Escala Log)")
+# plt.grid(True, which="both")
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
